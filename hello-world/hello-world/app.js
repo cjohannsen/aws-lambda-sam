323866fee1bc721bd30908d87dc5231fde2571c3
Maximilian Röll <maximilian.roell@plentymarkets.com>
2021-07-29 17:46:27 +0200

    add lambda vsf

--- hello-world/hello-world/app.js
+++ hello-world/hello-world/app.js
@@ -1,34 +1,50 @@
-// const { renderVueApp } = require("./src/vueRenderer")
-const { renderCeres } = require("./src/ceresRenderer")
+const aws = require("aws-sdk");
+const s3 = new aws.S3();
+const fs = require("fs");
 
-let response
+const { loadNuxt } = require("nuxt");
 
-exports.lambdaHandler = async (event, context) => {
-    try {
-        // const html = await renderVueApp()
-        const ceresHTML = await renderCeres()
+const decompress = require("decompress");
+const decompressTargz = require("decompress-targz");
+const serverEntryZip = "nuxt.tar.gz";
 
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                ceresHTML
-            })
-        }
+exports.handler = async (event, context) => {
+  const bucket = "ceres-lambda-ssr";
+  const key = `vuestorefront/${serverEntryZip}`;
 
-        // response = {
-        //     statusCode: 200,
-        //     body: JSON.stringify(obj),
-        //     headers: {
-        //         "content-type": [{ 
-        //             key: "Content-Type",
-        //             value: "application/json charset=utf-8"
-        //         }]
-        //     }
-        // }
-    } catch (err) {
-        console.log(err)
-        return err
-    }
+  // Get the object from the event and show its content type
+  const params = {
+    Bucket: bucket,
+    Key: key,
+  };
+  try {
+    const { ContentType, Body } = await s3.getObject(params).promise();
+    console.log("CONTENT TYPE:", ContentType);
+    fs.writeFileSync(`/tmp/${serverEntryZip}`, Body);
 
-    return response
-}
+    const test = await decompress(`/tmp/${serverEntryZip}`, "/tmp/", {
+      plugins: [decompressTargz()],
+    });
+
+    let _files = [];
+    _files = fs.readdirSync("/tmp/.nuxt");
+    console.log(_files);
+
+    const nuxt = await loadNuxt(
+    { 
+        for: "start",
+        rootDir: '/tmp'
+    });
+
+    const { html, error, redirected } = await nuxt.renderRoute("/home");
+
+    return {
+      body: html,
+    };
+  } catch (err) {
+    console.log(err);
+    const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+    console.log(message);
+    throw new Error(message);
+  }
+};
